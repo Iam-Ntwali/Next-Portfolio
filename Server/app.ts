fc188c2298a0d16c5de48f8e1f4861d8273cf048
Iam-Ntwali <ntwalipit@gmail.com>
2024-04-30 20:58:27 +0200

    some changes

--- Server/app.ts
+++ Server/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -8,3 +9,18 @@ app.use(express.json({ limit: "50mb" }));
 
 // cookies parser
 app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
