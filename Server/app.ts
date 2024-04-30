4960b8cfccf4957a7451bc85a3c245d5776b4f64
Iam-Ntwali <ntwalipit@gmail.com>
2024-04-30 20:43:11 +0200

    some changes

--- Server/app.ts
+++ Server/app.ts
@@ -1,2 +1,10 @@
 import express from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookies parser
+app.use(cookieParser());
