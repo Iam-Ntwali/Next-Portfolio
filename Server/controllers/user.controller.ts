05d01ab373c5d33cf552b77483031415eb086311
Iam-Ntwali <ntwalipit@gmail.com>
2024-05-05 19:14:51 +0200

    some changes

--- Server/controllers/user.controller.ts
+++ Server/controllers/user.controller.ts
@@ -128,3 +128,32 @@ export const activateUser = catchAsyncError(
     }
   }
 );
+
+// Login user
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body as ILoginRequest;
+      if (!email || !password) {
+        return next(new ErrorHandler("Please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({ email }).select("+password");
+      if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+    } catch (err: any) {
+      return next(new ErrorHandler(err.message, 400));
+    }
+  }
+);
