17322be0364b1e57909b23e969112dd1891cda96
Iam-Ntwali <ntwalipit@gmail.com>
2024-05-18 10:32:59 +0200

    feat: update notification status

--- Server/controllers/notification.controller.ts
+++ Server/controllers/notification.controller.ts
@@ -19,3 +19,32 @@ export const getNotifications = catchAsyncError(
     }
   }
 );
+
+// update notification status --- only admin
+export const updateNotificationStatus = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notification = await NotificationModel.findById(req.params.id);
+      if (!notification) {
+        return next(new ErrorHandler("Notification not found", 404));
+      } else {
+        notification.status
+          ? (notification.status = "read")
+          : notification?.status;
+      }
+
+      await notification.save();
+
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1,
+      });
+
+      res.status(201).json({
+        success: true,
+        notifications,
+      });
+    } catch (err: any) {
+      return next(new ErrorHandler(err.message, 500));
+    }
+  }
+);
