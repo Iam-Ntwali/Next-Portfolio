bd6bee259f973413ef3d50233759499986e4d9e5
Iam-Ntwali <ntwalipit@gmail.com>
2024-05-21 21:43:17 +0200

    some changes

--- client/app/utils/NavItems.tsx
+++ client/app/utils/NavItems.tsx
@@ -1,6 +1,31 @@
 import React from "react";
 
-type Props = {};
+export const NavItemsData = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Courses",
+    url: "/courses",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Policy",
+    url: "/policy",
+  },
+  {
+    name: "FAQ",
+    url: "/faq",
+  },
+];
+type Props = {
+  activeItem: number;
+  isMobile: boolean;
+};
 const NavItems = (props: Props) => {
   return <div>NavItems</div>;
 };
