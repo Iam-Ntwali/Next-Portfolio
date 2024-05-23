1dcbcbba4c823d95c7a2901bff7972aace765b7d
Iam-Ntwali <ntwalipit@gmail.com>
2024-05-23 22:39:57 +0200

    some changes

---
+++ client/app/components/Route/Hero.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BiSearch } from "react-icons/bi";
+
+type Props = {};
+
+const Hero: FC<Props> = (props) => {
+  return <div className="w-full 1000px:flex items-center"></div>;
+};
+
+export default Hero;
