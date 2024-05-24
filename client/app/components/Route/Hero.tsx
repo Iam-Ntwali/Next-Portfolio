23572ca499f4a615d76d313566c7116148159f6e
Iam-Ntwali <ntwalipit@gmail.com>
2024-05-24 19:55:02 +0200

    some changes

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -6,7 +6,19 @@ import { BiSearch } from "react-icons/bi";
 type Props = {};
 
 const Hero: FC<Props> = (props) => {
-  return <div className="w-full 1000px:flex items-center"></div>;
+  return (
+    <div className="w-full 1000px:flex items-center">
+      <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:w-[600px] h-[50vh] w-[50vh] hero-animation">
+        <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
+          <Image
+            src={require("../../../public/assets/ibtc-hero1.png")}
+            alt="hero image"
+            className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
