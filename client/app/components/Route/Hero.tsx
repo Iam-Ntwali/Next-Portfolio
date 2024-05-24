b2d24f6664394b83d6c567169cd276f664eed8c5
Iam-Ntwali <ntwalipit@gmail.com>
2024-05-24 20:08:00 +0200

    some changes

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const Hero: FC<Props> = (props) => {
   return (
     <div className="w-full 1000px:flex items-center">
-      <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:w-[600px] h-[50vh] w-[50vh] hero-animation">
+      <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:w-[600px] h-[50vh] w-[50vh] hero-animation ">
         <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
           <Image
             src={require("../../../public/assets/ibtc-hero1.png")}
@@ -17,9 +17,11 @@ const Hero: FC<Props> = (props) => {
           />
         </div>
         <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-          <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[0]">
+          <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px]">
             Improve your Online Learning Experience Better Instantly
           </h2>
+          <br />
+          <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18] 1500px:!w-[55%] 1100px:!w-[78%]"></p>
         </div>
       </div>
     </div>
