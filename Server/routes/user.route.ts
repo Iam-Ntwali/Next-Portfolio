e6ec4885edc0f0b92847bc321a12fe665679aa9b
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-04 16:44:37 +0200

    added some comments in user route.ts

--- Server/routes/user.route.ts
+++ Server/routes/user.route.ts
@@ -18,28 +18,53 @@ import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const userRouter = express.Router();
 
+// Create user
 userRouter.post("/registration", registrationUser);
+
+// Activate user
 userRouter.post("/activate-user", activateUser);
+
+// Login user
 userRouter.post("/login", loginUser);
+
+// Logout user
 userRouter.get("/logout", isAuthenticated, logoutUser);
+
+// Refresh access token
 userRouter.get("/refresh", updateAccessToken);
-userRouter.get("/user-info", isAuthenticated, getUserInfo);
+
+// Social auth login
 userRouter.post("/social-auth", socialAuth);
+
+// Get user info
+userRouter.get("/user-info", isAuthenticated, getUserInfo);
+
+// Update user info
 userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+
+// Update user password
 userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+
+// Update user avatar
 userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
+
+// Get all users in DB
 userRouter.get(
   "/get-all-users",
   isAuthenticated,
   authorizeRoles("admin"),
   getAllUsers
 );
+
+// Update user role
 userRouter.put(
   "/update-user-role",
   isAuthenticated,
   authorizeRoles("admin"),
   updateUserRole
 );
+
+// Delete user
 userRouter.delete(
   "/delete-user/:id",
   isAuthenticated,
