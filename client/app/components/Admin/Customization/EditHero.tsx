10fecc329c88519a6670f9c959c8d93434776398
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-05 18:19:26 +0200

    added new component to edit hero

---
+++ client/app/components/Admin/Customization/EditHero.tsx
@@ -0,0 +1,16 @@
+// import {
+//   useEditLayoutMutation,
+//   useGetHeroDataQuery,
+// } from "@/redux/features/layout/layoutApi";
+import React, { FC, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { AiOutlineCamera } from "react-icons/ai";
+import { styles } from "../../../../app/styles/style";
+
+type Props = {};
+
+const EditHero: FC<Props> = (props: Props) => {
+  return <div>EditHero</div>;
+};
+
+export default EditHero;
