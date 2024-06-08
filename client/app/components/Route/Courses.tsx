69e28369cf0d4e450f7b2a4d8045c898d18ff7f2
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-08 18:11:22 +0200

    added course card component

--- client/app/components/Route/Courses.tsx
+++ client/app/components/Route/Courses.tsx
@@ -1,6 +1,6 @@
-import { useGetUsersAllCoursesQuery } from "@/redux/features/courses/coursesApi";
+import { useGetUsersAllCoursesQuery } from "../../../redux/features/courses/coursesApi";
 import React, { useEffect, useState } from "react";
-// import CourseCard from "../Course/CourseCard";
+import CourseCard from "../Course/CourseCard";
 
 type Props = {};
 
@@ -18,15 +18,15 @@ const Courses = (props: Props) => {
         <h1 className="text-center font-Poppins text-[25px] leading-[35px] sm:text-3xl lg:text-4xl dark:text-white 800px:!leading-[60px] text-[#000] font-[700] tracking-tight">
           Expand Your Career <span className="text-gradient">Opportunity</span>{" "}
           <br />
-          Opportunity With Our Courses
+          With Our Courses
         </h1>
         <br />
         <br />
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
-          {/* {courses &&
+          {courses &&
             courses.map((item: any, index: number) => (
               <CourseCard item={item} key={index} />
-            ))} */}
+            ))}
         </div>
       </div>
     </div>
