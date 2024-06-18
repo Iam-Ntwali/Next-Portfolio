b956ef7ba7709f657a9cea6e3ce7e56824b21db3
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-18 17:42:16 +0200

    some chnages

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,5 +1,43 @@
+"use client";
 import React from "react";
+import Title from "./Title";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import {
+  SiExpress,
+  SiGit,
+  SiJavascript,
+  SiMongoose,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiSupabase,
+  SiTailwindcss,
+} from "react-icons/si";
+import { text } from "stream/consumers";
 
 export default function Skills() {
-  return <div>Skills</div>;
+  // Skills Variable
+  const skills = [
+    { text: "Javascript", Icon: SiJavascript },
+    { text: "Tailwind", Icon: SiTailwindcss },
+    { text: "Node.js", Icon: SiNodedotjs },
+    { text: "Express", Icon: SiExpress },
+    { text: "React", Icon: SiReact },
+    { text: "Next.js", Icon: SiNextdotjs },
+    { text: "Git", Icon: SiGit },
+    { text: "Supabase", Icon: SiSupabase },
+    { text: "Mongo DB", Icon: SiMongoose },
+  ];
+  return (
+    <div className="max-w-5xl mx-auto px-8">
+      {/* Section title */}
+      <Title
+        text="Skills 🔪"
+        className="flex flex-col items-center justify-center -rotate-6"
+      />
+
+      {/* Content cards */}
+      <HoverEffect items={skills} />
+    </div>
+  );
 }
