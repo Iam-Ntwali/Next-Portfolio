d1232389fa892d8f413e8ed9ff6e5164d7da0047
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-18 19:37:29 +0200

    some changes

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { SiNextdotjs, SiReactquery, SiTailwindcss } from "react-icons/si";
 import Title from "./Title";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default function Projects() {
   const projects = [
@@ -42,8 +44,18 @@ export default function Projects() {
       {/* Section title */}
       <Title
         text="Projects 🎨"
-        className="flex flex-col items-center justify-center -rotate-6"
+        className="flex flex-col items-center justify-center rotate-6"
       />
+
+      <div className="grid grid-cols-1 sm:grids-cols-2 pt-20">
+        {projects.map((project, index) => {
+          return (
+            <Link href={project.github} key={index}>
+              <div className={cn("p-5 rounded-md", project.background)}></div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
