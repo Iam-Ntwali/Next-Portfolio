6f94e9a42f6da23fdf5879f8776a64c83f64781c
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-18 15:59:25 +0200

    added grid backgroud

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,9 +5,11 @@ import HeroSection from "./components/HeroSection";
 export default function page() {
   return (
     <div className="min-h-screen bg-black overflow-hidden">
-      <div className="max-w-7xl mx-auto p-5">
-        <Navbar />
-        <HeroSection />
+      <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2]">
+        <div className="max-w-7xl mx-auto p-5">
+          <Navbar />
+          <HeroSection />
+        </div>
       </div>
     </div>
   );
